0bbfef807f559a223757d4c9a8295bb36579ba86
ekntrtmz <hi@ekntrtmz.com>
2019-12-15 23:23:25 +0100

    Rename user to username

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -81,7 +81,7 @@
                 @else
                 <div class="navbar-item has-dropdown is-hoverable">
                     <a class="navbar-link">
-                        {{ Auth::user()->name }}
+                        {{ Auth::user()->username }}
                     </a>
                     <div class="navbar-dropdown">
                         <a class="navbar-item">
