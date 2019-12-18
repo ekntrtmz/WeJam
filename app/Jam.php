961725e632078f46524f1d623721ff877ac6f725
ekntrtmz <hi@ekntrtmz.com>
2019-12-18 18:30:57 +0100

    Add relationsships

--- app/Jam.php
+++ app/Jam.php
@@ -4,7 +4,18 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+
 class Jam extends Model
 {
     //
+
+    public function city()
+    {
+        return $this->belongsTo(City::class);
+    }
+
+    public function owner()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
