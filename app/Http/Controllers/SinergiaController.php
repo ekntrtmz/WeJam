3a84add21626f84ef155600618bfbdee03af22bc
ekntrtmz <hi@ekntrtmz.com>
2019-12-18 19:31:41 +0100

    Add Sinergia model -m -c

--- app/Http/Controllers/SinergiaController.php
+++ app/Http/Controllers/SinergiaController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class CollaborationController extends Controller
+class SinergiaController extends Controller
 {
     //
 }
--- app/Sinergia.php
+++ app/Sinergia.php
@@ -4,7 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Collaboration extends Model
+class Sinergia extends Model
 {
     //
 }
