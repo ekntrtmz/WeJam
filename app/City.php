eb9255144c6633b8b840c3c9573d8260c3d4c4fe
ekntrtmz <hi@ekntrtmz.com>
2019-12-19 13:19:12 +0100

    Change Route Key Name to 'short'

--- app/City.php
+++ app/City.php
@@ -7,9 +7,16 @@ use Illuminate\Database\Eloquent\Model;
 class City extends Model
 {
     //
-
+    //  Relationsship to Jam
+    //  used in cities.cities and ciities.city
     public function jams()
     {
         return $this->hasMany(Jam::class);
+    }
+
+    //  RouteModelBinding with different key/parameter : 'short' instead 'id' (default)
+    public function getRouteKeyName()
+    {
+        return 'short';
     }
 }
