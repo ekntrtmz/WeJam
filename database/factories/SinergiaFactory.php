3e74a5539a0d42adad6a95950e335a763a8abb65
ekntrtmz <hi@ekntrtmz.com>
2019-12-19 15:14:26 +0100

    Add factory for Sinergia

---
+++ database/factories/SinergiaFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Sinergia;
+use Faker\Generator as Faker;
+
+$factory->define(Sinergia::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
