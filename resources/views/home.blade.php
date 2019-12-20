796d52e0d037432e4db74a6ebe3a3237519bb9f9
ekntrtmz <hi@ekntrtmz.com>
2019-12-20 18:38:17 +0100

    Start with data view

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,39 @@
 @extends('layouts.app')
 
 @section('content')
+
+@if (session('status'))
+    <div class="notification is-success">
+        {{ session('status') }}
+    </div>
+@endif
+
 <div class="columns is-multiline">
     <div class="column is-12">
-            <h1 class="title">Home</h1>
+    <h1 class="title">{{__('Next')}}</h1>
     </div>
     <div class="column is-12">
         <div class="box">
-                @if (session('status'))
-                <div class="notification is-success">
-                    {{ session('status') }}
-                </div>
-            @endif
-
-            Hello {{ Auth::user()->username }},<br>
-            You are logged in!
-
-
+            @foreach(Auth::user()->jams as $jam)
+            {{$jam->title}}
+            @endforeach
         </div>
+    </div>
+</div>
+<div class="columns is-multiline">
+    <div class="column is-12">
+        <h1 class="title">{{__('Recommended')}}</h1>
+    </div>
+    <div class="column is-12">
+        <div class="box"></div>
+    </div>
+</div>
+<div class="columns is-multiline">
+    <div class="column is-12">
+    <h1 class="title">{{__('History')}}</h1>
+    </div>
+    <div class="column is-12">
+        <div class="box"></div>
     </div>
 </div>
 @endsection
