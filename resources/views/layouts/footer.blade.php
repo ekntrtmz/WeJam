de0fa629a56e8936baa365a6716b41db45579529
ekntrtmz <hi@ekntrtmz.com>
2019-12-23 12:27:04 +0100

    Add footer

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,10 @@
+<footer class="footer container">
+  <div class="content columns">
+    <div class="column has-text-white">
+      <div class="branding">wejam</div>
+      <div class="slogan">Connect with real people and real moments.</p>
+    </div>
+    <div class="column"></div>
+    <div class="column"></div>
+  </div>
+</footer>
